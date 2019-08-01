4c52f02163526dfa1956c01424aeb84387d9d661
raizensmith <em.raizensmith@gmail.com>
2019-08-01 12:38:43 +0700

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['prefix' => 'admin', 'middleare' => 'auth'], 
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], 
 function () {
     Route::get('/', function () {
         return view('backend.index');
